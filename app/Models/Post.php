8399cf2517c64490745ed844a519323b8a1d765d
Marouane-Ch21 <contactmarouanechqief@gmail.com>
2024-04-28 13:25:08 +0100

    theme installation & fetching data with eager loading

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
@@ -12,8 +13,13 @@ class Post extends Model
 
     protected $fillable = ['title', 'conetent'];
 
-    public function comments() : HasMany
+    public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,18 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Route::middleware(['auth'])->prefix('admin')->group(function () {
     Route::get('/', function () {
         return view('admin.index');
     })->name('admin.dashboard.index');
 
-    Route::get('/posts', function () {
-        return view('admin.posts.index');
-    })->name('admin.posts.index');
+    Route::get('/posts', [PostController::class, 'index'])->name('admin.posts.index');
 });
 
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
